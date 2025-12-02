49a68ee4eed5c330ac38c4315a5637f944383b3d
Nisou624 <anistilmatine6@gmail.com>
2025-12-02 08:27:29 +0100

    interractive map functionnal

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-
-// Import des pages
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Implantations from './pages/Implantations'
+import PageTransition from './components/PageTransition/PageTransition';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Projects from './pages/Projects';
-import Implantations from './pages/Implantations';
 import Blog from './pages/Blog';
 import FAQ from './pages/FAQ';
+import './App.css';
 
-// Import des composants
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+function AppContent() {
+  const location = useLocation();
 
-function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
+    <div className="App">
+      <Header />
+      <main id="main" className="site-main">
+        <PageTransition location={location.pathname}>
+          <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/projects" element={<Projects />} />
-            <Route path="/implantations" element={<Implantations />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/faq" element={<FAQ />} />
+            <Route path="/implantations" element={<Implantations />} />
           </Routes>
-        </main>
-        <Footer />
-      </div>
+        </PageTransition>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
